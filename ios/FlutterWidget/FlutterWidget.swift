175ec58d864be599bfbacfd46f07ec845b501ae2
Cameron King <cameronyking@gmail.com>
2020-11-23 01:40:06 -0800

    archive error

---
+++ ios/FlutterWidget/FlutterWidget.swift
@@ -0,0 +1,101 @@
+//
+//  FlutterWidget.swift
+//  FlutterWidget
+//
+//  Created by Thomas Leiter on 28.10.20.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+import CoreData
+
+struct FlutterData: Decodable, Hashable {
+    let text: String
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let flutterData: FlutterData?
+}
+
+struct Provider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), flutterData: FlutterData(text: "Hello from Flutter"))
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), flutterData: FlutterData(text: "Hello from Flutter"))
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+        
+        let sharedDefaults = UserDefaults.init(suiteName: "group.com.fasky")
+        var flutterData: FlutterData? = nil
+        
+        if(sharedDefaults != nil) {
+            do {
+              let shared = sharedDefaults?.string(forKey: "widgetData")
+              if(shared != nil){
+                let decoder = JSONDecoder()
+                flutterData = try decoder.decode(FlutterData.self, from: shared!.data(using: .utf8)!)
+              }
+            } catch {
+              print(error)
+            }
+        }
+
+        let currentDate = Date()
+        let entryDate = Calendar.current.date(byAdding: .hour, value: 24, to: currentDate)!
+        let entry = SimpleEntry(date: entryDate, flutterData: flutterData)
+        entries.append(entry)
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct FlutterWidgetEntryView : View {
+    var entry: Provider.Entry
+    
+    
+    private var FlutterDataView: some View {
+        
+        QRCodeView(code: entry.flutterData!.text)
+    }
+    
+    private var NoDataView: some View {
+      Text("No Data found! Go to the Flutter App")
+    }
+    
+    var body: some View {
+      if(entry.flutterData == nil) {
+        NoDataView
+      } else {
+        FlutterDataView
+      }
+    }
+}
+
+
+@main
+struct FlutterWidget: Widget {
+    let kind: String = "Dappy Widget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            FlutterWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Dappy Contact Widget")
+        .description("This is a widget to quickly display Dappy QR code.")
+    }
+}
+
+struct FlutterWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        FlutterWidgetEntryView(entry: SimpleEntry(date: Date(), flutterData: nil))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
